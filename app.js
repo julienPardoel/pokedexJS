06ac31c31b3f3d146a2b0923bee835893b3c1671
julien <julien.pardoel@gmail.com>
2022-11-17 23:46:21 +0100

    fetch pokeAPI

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const grid = document.getElementById("grid");
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  .then((response) => response.json())
+  .then((data) => {
+    const results = data.results;
+    // console.log(results);
+    results.forEach((pokemon, index) => {
+      // item
+      const ceil = document.createElement("div");
+      ceil.classList.add("item");
+      const pokemonNumber = ++index;
+      //   img
+      const img = document.createElement("img");
+      img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonNumber}.png`;
+      //   name
+      const name = document.createElement("div");
+      const pokemonName = pokemon.name;
+      name.innerText = pokemonName;
+      //   newItem
+      ceil.appendChild(img);
+      ceil.appendChild(name);
+      grid.appendChild(ceil);
+    });
+  });
